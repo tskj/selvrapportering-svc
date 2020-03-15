35240a16f9d5a0bcbd86d5f73dbf56fbcdf5d7e6
tarjei <tarjei.skjaerset@gmail.com>
2020-03-15 21:44:16 +0100

    Refactor to be a list of "file" objects; a name-content tuple

--- src/migrations.ts
+++ src/migrations.ts
@@ -1,6 +1,11 @@
 import fs from 'fs';
 import { client } from './api/repository';
 
+interface File {
+  name: string;
+  content: string;
+}
+
 export const migrate = async () => {
   await client.query(
     'CREATE TABLE IF NOT EXISTS Migrations (Name varchar(255))'
@@ -13,28 +18,30 @@ export const migrate = async () => {
   const allMigrations = await getMigrations();
 
   const notRunMigrations = allMigrations
-    .filter(([name, _]) => !finishedMigrations.map(x => x.name).includes(name))
-    .sort(([name1], [name2]) => (name1 < name2 ? -1 : name2 < name1 ? 1 : 0));
+    .filter(({ name }) => !finishedMigrations.map(x => x.name).includes(name))
+    .sort(({ name: name1 }, { name: name2 }) =>
+      alphabeticalOrder(name1, name2)
+    );
 
   console.log('Fant følgende migrasjoner:');
   console.log(notRunMigrations);
 
-  notRunMigrations.forEach(async ([name, migration]) => {
+  notRunMigrations.forEach(async ({ name, content: migration }) => {
     await client.query(migration);
     client.query('INSERT INTO Migrations (Name) VALUES ($1)', [name]);
   });
 };
 
-const getMigrations = async (): Promise<string[][]> => {
+const getMigrations = async (): Promise<File[]> => {
   const path = './dist/migrations';
   return new Promise(resolve =>
     fs.readdir(path, (err, fileNames) => {
       resolve(
         Promise.all(
-          fileNames.map(async fileName => [
-            fileName,
-            await readFileContent(path + '/' + fileName),
-          ])
+          fileNames.map(async fileName => ({
+            name: fileName,
+            content: await readFileContent(path + '/' + fileName),
+          }))
         )
       );
     })
@@ -46,3 +53,6 @@ const readFileContent = (fileName: string): Promise<string> => {
     fs.readFile(fileName, 'utf8', (err, data) => resolve(data))
   );
 };
+
+const alphabeticalOrder = (str1: string, str2: string) =>
+  str1 < str2 ? 1 : str2 < str1 ? -1 : 0;
