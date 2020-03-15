2d3f6f5616cb65c56290812689a04f0b4707b876
Jørgen Granseth <jorgen.granseth@bekk.no>
2020-03-15 17:47:18 +0100

    Basic Postgres-oppsett

---
+++ src/api/repository.ts
@@ -0,0 +1,39 @@
+import { Pool, Client } from 'pg';
+
+const connectionString = 'postgresql://user:password@localhost:5432/selvrapportering';
+
+const client = new Client({
+    connectionString,
+});
+client.connect();
+
+export async function migrateV1() {
+    const initTable = `
+        CREATE TABLE "person" (
+            id          SERIAL PRIMARY KEY,
+            name        VARCHAR(255)
+        );
+    `;
+    return await client.query(initTable);
+}
+
+export async function selectNow() {
+    const date =  await client.query('SELECT NOW()');
+    return date.rows[0];
+}
+
+export async function createUser(name: string) {
+    const text = "INSERT INTO person (name) VALUES($1) RETURNING *";
+    const values = [name];
+    const sqlRes = await client.query(text, values);
+    console.log(sqlRes);
+    return sqlRes.rows[0];
+}
+
+export async function getUser(id: number) {
+    const text = "SELECT * FROM person WHERE id = $1";
+    const values = [id];
+    const sqlRes = await client.query(text, values);
+    console.log(sqlRes);
+    return sqlRes.rows;
+}
